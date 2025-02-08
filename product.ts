128229eb557f8f0c2c6aaf2b7e1d35bcb1a3cdf9
ANOOSHA-QASIM <anushatech333@gmail.com>
2025-02-08 23:15:05 +0500

    Initial setup of Next.js e-commerce project

---
+++ product.ts
@@ -0,0 +1,12 @@
+// export interface Product {
+//     _id:string,
+//     name:string,
+//    "slug": slug.current,
+//     price:number,
+//     description,
+//     "imageUrl": image.asset->url,
+//     discountPercentage,
+//     isFeaturedProduct,
+//     stockLevel,
+//     category
+// }
